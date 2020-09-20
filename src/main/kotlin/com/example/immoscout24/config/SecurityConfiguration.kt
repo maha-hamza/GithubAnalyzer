911bfd9170e2a96c819ac1fd05bf5442b2a1691a
maha <maha-hamza@users.noreply.github.com>
2020-09-21 00:26:07 +0200

    Optimize imports

--- src/main/kotlin/com/example/immoscout24/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/example/immoscout24/config/SecurityConfiguration.kt
@@ -1,14 +1,10 @@
 package com.example.immoscout24.config
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 
 @Configuration
--- src/main/kotlin/com/example/immoscout24/controller/HtmlController.kt
+++ src/main/kotlin/com/example/immoscout24/controller/HtmlController.kt
@@ -14,7 +14,6 @@ import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.servlet.ModelAndView
 
 
 @Controller
